c32e69f0c6fb6f4e600f5ec201a6329dd0b8efed
RusanDenis <rusandenis18@gmail.com>
2025-08-13 19:52:56 +0300

    add bonus

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+int count = 15000;
+int promotion = 20;
+int bonus = count/promotion;
+System.out.println(bonus);
+
+    }
+}
